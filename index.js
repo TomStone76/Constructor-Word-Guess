ba30b19572dfb531e410046d722f2077b0692d88
YQS <41525397+TomStone76@users.noreply.github.com>
2018-10-16 14:07:20 -0400

    commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var inquirer = require("inquirer");
 
-var randChoice = Math.floor(Math.random() * 51);
-
 var arr = ['Alabama','Alaska', 'Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia', 'Florida',
 'Georgia', 'Hawaii', 'Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Maryland','Massachusetts','Michigan',
 'Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina',
 'North Dakota','Ohio','Oklahoma','Oregon','Pennsylvania','Rhode Island', 'South Carolina','South Dakota','Tennessee','Texas','Utah',
-'Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'],
+'Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'];
 
-for (var i = 0; i < arr.length; i++) {
+var randState = arr[Math.floor(Math.random() * arr.length)];
 
-}
+// for (var i = 0; i < arr.length; i++) {
+
+// }
+
+console.log(randState);
